558c4ad5967e8b4558e232153d7d86746cb6aa24
Ricky Reusser <rsreusser@gmail.com>
2015-12-07 15:17:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# react-native-paged-scroll-view
+
+> A higher-order React Native component to compute the current and total pages of a ScrollView-compatible component
+
+## Introduction
+
+This module implements a [higher-order component](https://gist.github.com/sebmarkbage/ef0bf1f338a7182b6775) that computes the current and total pages contained in a React Native [ScrollView](https://facebook.github.io/react-native/docs/scrollview.html) (or functionally similar) component. So it's really very simple but attempts to solve layout race conditions, re-layout and other subtleties. This component could trivially be used as a swiper alongside a page indicator but does not implement that itself.
+
+
+## Notes
+
+This component is currently very useful, but needs a bit of additional work to ensure it's not triggering unnecessary state changes and that there are no race conditions.
+
+## Installation
+
+```bash
+$ npm install react-native-paged-scroll-view
+```
+
+## Example
+
+```javascript
+import { ScrollView } from 'react-native'
+import AddPaging from 'react-native-paged-scroll-view'
+var PagedScrollView = AddPaging(ScrollView)
+
+  ...
+  handlePageChange (state) {
+    // Triggered on layout or when the page state changes:
+    console.log('current horizontal page:', state.currentHorizontalPage)
+    console.log('current vertical page:  ', state.currentVerticalPage)
+    console.log('total horizontal pages: ', state.totalHorizontalPages)
+    console.log('total vertical pages:   ', state.totalVerticalPages)
+  }
+
+  anotherMethod () {
+    // Can also query imperatively via ref:
+    this.refs.PagedScrollView.state.currentHorizontalPage
+  }
+
+  render () {
+    return (
+      <PagedScrollView
+        ref="PagedScrollView"
+        onPageChange={this.handlePageChange.bind(this)}
+      >
+        ...
+      </PagedScrollView>
+    )
+  }
+  ...
+```
+
+
+## Usage
+
+#### `require('react-native-paged-scroll-view')(Component)`
+Wrap either a `ScrollView` or a component functionally equivalent (implements `onScroll` and similar basic methods). Returns a higher order component with props passed through.
+
+**Props**:
+- `onPageChange`: `function(state)`: Executed on initial layout, when the page changes, or when the inner content changes. Callback is passed `state` object containing:
+  - `totalHorizontalPages`: total number of horizontal pages, rounded to the nearest integer.
+  - `totalVerticalPages`: total number of vertical pages, rounded to the nearest integer.
+  - `currentHorizontalPage`: the current horizontal page, rounded to the nearest integer.
+  - `currentVerticalPage`: the current vertical page, rounded to the nearest integer.
+
+**Attributes**:
+The above props may be accesed on `ref.state.*` in addition to:
+- `ref.scrollX`: current horizontal scroll offset
+- `ref.scrollY`: current vertical scroll offset
+
+
+# License
+(c) 2015 Ricky Reusser. MIT License.
---
+++ index.js
@@ -0,0 +1,149 @@
+'use strict'
+
+import React from 'react-native'
+
+var AddPaging = ComposedComponent => class extends React.Component {
+
+  constructor () {
+    super(props)
+
+    // Important to remember these, but they're not really 'state' variables:
+    this.scrollX = 0
+    this.scrollY = 0
+
+    // We'll consider these state variables although maybe we shouldn't:
+    this.state = {
+      totalHorizontalPages: 0,
+      totalVerticalPages: 0,
+      currentHorizontalPage: null,
+      currentVerticalPage: null,
+    }
+  }
+
+  handleScroll (event) {
+    // Still trigger the passed callback, if provided:
+    this.props.onScroll && this.props.onScroll(event)
+
+    var e = event.nativeEvent
+
+    // Get values from event:
+    this.scrollViewWidth = e.layoutMeasurement.width
+    this.scrollViewHeight = e.layoutMeasurement.height
+    this.innerScrollViewWidth = e.contentSize.width
+    this.innerScrollViewHeight = e.contentSize.height
+
+    // These are important, but they're not state variables that trigger an update:
+    this.scrollX = e.contentOffset.x
+    this.scrollY = e.contentOffset.y
+
+    var totalHorizontalPages = Math.floor(this.innerScrollViewWidth / this.scrollViewWidth + 0.5)
+    var totalVerticalPages   = Math.floor(this.innerScrollViewHeight / this.scrollViewHeight + 0.5)
+
+    this.setState({
+      totalHorizontalPages:  totalHorizontalPages,
+      totalVerticalPages:    totalVerticalPages,
+      currentHorizontalPage: Math.min(Math.max(Math.floor(this.scrollX / this.scrollViewWidth + 0.5) + 1, 0), totalHorizontalPages),
+      currentVerticalPage:   Math.min(Math.max(Math.floor(this.scrollY / this.scrollViewHeight + 0.5) + 1, 0), totalVerticalPages)
+    })
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    if (this.props.onPageChange) {
+      var sendEvent = false
+      for (key in nextState) {
+        if (nextState.hasOwnProperty(key)) {
+          if (this.state[key] !== nextState[key]) {
+            sendEvent = true
+          }
+        }
+      }
+      if (sendEvent) {
+        this.props.onPageChange(nextState)
+      }
+    }
+  }
+
+  measureScrollView (cb) {
+    this._scrollView.refs.ScrollView.measure((x,y,w,h) => {
+      this.scrollViewWidth = w
+      this.scrollViewHeight = h
+      cb && cb()
+    })
+  }
+
+  measureInnerScrollView (cb) {
+    this._scrollView.refs.InnerScrollView.measure((x,y,w,h) => {
+      this.innerScrollViewWidth = w
+      this.innerScrollViewHeight = h
+      cb && cb()
+    })
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      var succeededCbs = 0
+      var totalCbs = 2
+
+      var computeNewState = () => {
+        if (++succeededCbs < totalCbs) return
+        var totalHorizontalPages = Math.floor(this.innerScrollViewWidth / this.scrollViewWidth + 0.5)
+        var totalVerticalPages   = Math.floor(this.innerScrollViewHeight / this.scrollViewHeight + 0.5)
+
+        this.setState({
+          totalHorizontalPages:  totalHorizontalPages,
+          totalVerticalPages:    totalVerticalPages,
+          currentHorizontalPage: Math.min(Math.max(Math.floor(this.scrollX / this.scrollViewWidth + 0.5) + 1, 0), totalHorizontalPages),
+          currentVerticalPage:   Math.min(Math.max(Math.floor(this.scrollY / this.scrollViewHeight + 0.5) + 1, 0), totalVerticalPages)
+        })
+      }
+
+      // Trigger both measurements at the same time and compute the new state only
+      // once they've both returned.
+      this.measureInnerScrollView(computeNewState.bind(this))
+      this.measureScrollView(computeNewState.bind(this))
+    })
+  }
+
+  handleContentSizeChange (event) {
+    this.props.onContentSizeChange && this.props.onContentSizeChange(event)
+
+    var e = event.nativeEvent
+
+    // Get values from event:
+    this.scrollViewWidth = e.layoutMeasurement.width
+    this.scrollViewHeight = e.layoutMeasurement.height
+    this.innerScrollViewWidth = e.contentSize.width
+    this.innerScrollViewHeight = e.contentSize.height
+
+    var totalHorizontalPages = Math.floor(this.innerScrollViewWidth / this.scrollViewWidth + 0.5)
+    var totalVerticalPages   = Math.floor(this.innerScrollViewHeight / this.scrollViewHeight + 0.5)
+
+    this.setState({
+      totalHorizontalPages:  totalHorizontalPages,
+      totalVerticalPages:    totalVerticalPages,
+      currentHorizontalPage: Math.min(Math.max(Math.floor(this.scrollX / this.scrollViewWidth + 0.5) + 1, 0), totalHorizontalPages),
+      currentVerticalPage:   Math.min(Math.max(Math.floor(this.scrollY / this.scrollViewHeight + 0.5) + 1, 0), totalVerticalPages)
+    })
+  }
+
+  getInnerRef (c) {
+    this.props.innerRef && this.props.innerRef(c)
+    this._scrollView = c
+  }
+
+  render () {
+    return (
+      <ComposedComponent
+        scrollEventThrottle={this.props.scrollEventThrottle || 50}
+        {...this.props}
+        innerRef={this.getInnerRef.bind(this)}
+        onScroll={this.handleScroll.bind(this)}
+        onContentSizeChange={this.handleContentSizeChange.bind(this)}
+      >
+        {this.props.children}
+      </ComposedComponent>
+    )
+  }
+}
+
+export default AddPaging
